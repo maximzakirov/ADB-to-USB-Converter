09e16fc79d69d9946af809eb0d5162d7ff2d90ae
maximzakirov <maxim.zakirov1@gmail.com>
2018-11-29 01:49:20 -0500

    Adding new firmware

---
+++ Software/Builds/.dep/main.d
@@ -0,0 +1,17 @@
+Builds/main.o: main.c \
+ /usr/local/CrossPack-AVR-20131216/avr/include/stdio.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/io.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/sfr_defs.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/iotn2313.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/portpins.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/common.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/version.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/fuse.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/lock.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/interrupt.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/util/delay.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/inttypes.h \
+ /usr/local/CrossPack-AVR-20131216/lib/gcc/avr/4.8.1/include/stdint.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/stdint.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/util/delay_basic.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/math.h
